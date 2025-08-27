ce6518a115aa051377c86ce28e62dc1f675b8498
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2025-08-27 10:09:02 -0600

    update-factufacil

--- includes/backup- (2).bat
+++
@@ -1,6 +0,0 @@
-cls
-SET PGPASSWORD=Orellana
-pg_dump -U postgres -v -F c registro_academico > c:\wamp64\www\registro_academico.dump
-cls
-xcopy c:\wamp64\www\registro_academico.dump "G:\Mi unidad\CE10391\respaldo" /y
-cls
--- includes/backup- sgf_web.bat
+++
@@ -1,6 +0,0 @@
-cls
-SET PGPASSWORD=Orellana
-pg_dump -U postgres -v -F c sgf_web > c:\wamp64\www\sgf_web.dump
-cls
-xcopy c:\wamp64\www\sgf_web.dump "G:\Mi unidad\CE10391\respaldo" /y
-cls
